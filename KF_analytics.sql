f1c5b2c3be453d945145fee8acb220c35406cc22
kamiliyaz <139536793+kamiliyaz@users.noreply.github.com>
2024-02-21 22:01:44 +0700

    KF_analytics.sql

--- KF_analytics.sql
+++ KF_analytics.sql
@@ -5,16 +5,16 @@ WITH calculated_columns AS (
     t.transaction_id,
     t.price AS actual_price,
     t.discount_percentage,
-    -- Creating persentase_gross_laba column as specified
-    CASE
-      WHEN t.price <= 50000 THEN t.price * 0.1
-      WHEN t.price <= 100000 THEN t.price * 0.15
-      WHEN t.price <= 300000 THEN t.price * 0.2
-      WHEN t.price <= 500000 THEN t.price * 0.25
-      ELSE t.price * 0.3
-    END AS persentase_gross_laba,
-    -- Creating nett_sales column by subtracting actual price from discounted price
-    t.price - (t.price * t.discount_percentage) AS nett_sales
+   -- Creating persentase_gross_laba column as specified
+  CASE
+    WHEN t.price <= 50000 THEN 0.1
+    WHEN t.price <= 100000 THEN 0.15
+    WHEN t.price <= 300000 THEN 0.2
+    WHEN t.price <= 500000 THEN 0.25
+    ELSE 0.3
+  END AS persentase_gross_laba,
+    -- Creating nett_sales column by subtracting actual price from discounted price, and rounding it
+    CAST(t.price - (t.price * t.discount_percentage)AS INT64) AS nett_sales
   -- Selecting input columns from kf_final_transaction table (initial t)
   FROM `kimia_farma.kf_final_transaction` AS t
 )
@@ -34,8 +34,8 @@ SELECT
   cc.discount_percentage,
   cc.persentase_gross_laba,
   cc.nett_sales,
-  -- Calculating nett_profit since nett_sales column already exists
-  ((cc.nett_sales * cc.persentase_gross_laba) - cc.actual_price) AS nett_profit,
+  -- Calculating nett_profit and rounding it so the profit is not decimals
+  CAST(cc.nett_sales * cc.persentase_gross_laba AS INT64) AS nett_profit,
   t.rating AS rating_transaksi
 -- Selecting input columns from kf_final_transaction table (initial t)
 FROM `kimia_farma.kf_final_transaction` AS t
